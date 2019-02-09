3dbf45f730ec38b865c93d3dde19c5c396e6da40
나경식 <autona@naver.com>
2019-02-09 15:14:29 +0900

    null 연산 테스트

--- NullOperater/NullOperater/Program.cs
+++ NullOperater/NullOperater/Program.cs
@@ -4,32 +4,18 @@ namespace NullOperater
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            object a = null;
-            object b = "abc";
-
-            object c = a ?? b;
-            Console.WriteLine(c);
-
-            object d;
-
-            if (a != null)
-            {
-
-                d = a;
-            }
-            else {
-
-                d = b;
-            }
-
-            Console.WriteLine(d);
-
-            object e;
-            e = (a != null) ? a : d;
-
-            Console.WriteLine(e);
+            //Nullable Type, a 가 NULL 을 허용한다는 의미
+            int? a = null;
+            //int b = a.Value; // a.Value 값 자체가 null 이므로 int? b 도 오류
+            //Console.WriteLine("Value of b is {0}", b);
+            int b;
+            if (a.HasValue) b = a.Value;
+            else b = 0;
+            Console.WriteLine("Value of b is {0}", b);
+            int c = a ?? 0;
+            Console.WriteLine("Value of c is {0}", c);
+            Console.ReadLine();
         }
-    }
-}
+    }}
