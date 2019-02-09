bb0d9e59701740aa32d19e73ec57281729701c11
나경식 <autona@naver.com>
2019-02-09 13:59:01 +0900

    typecast 테스트

---
+++ ParameterTest/ParameterTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParameterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello {0},{1}", args[0], args[1]);
+        }
+    }
+}
