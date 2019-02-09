3e499a0c04c82c1e12eb65544afc3fde4025eb2a
나경식 <autona@naver.com>
2019-02-09 14:24:15 +0900

    산술연산자

---
+++ PulsMinusTrest/PulsMinusTrest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PulsMinusTrest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(+3);
+            Console.WriteLine(3 + 3);
+            Console.WriteLine(3 + .3);
+            Console.WriteLine("3" + "3");
+            Console.WriteLine(3.0 + "3");
+
+            Console.WriteLine(!true);
+
+            byte x = 10;
+            Console.WriteLine(~x);
+
+            Console.WriteLine(unchecked((short)50000));
+        }
+    }
+}
