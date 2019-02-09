c062e12882a3932d29ffe03f3a688bbd75fd63d9
나경식 <autona@naver.com>
2019-02-09 17:32:45 +0900

    for문

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int m;
+            while (i <=5)
+            {
+                m = 2 * i;
+                Console.Write(m + " " );
+                i++;
+            }
+
+        }
+    }
+}
