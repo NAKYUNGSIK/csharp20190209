96ff5ac79ae36f688180ce9dfd1e4d5c6c80cb71
나경식 <autona@naver.com>
2019-02-09 14:42:45 +0900

    회사 소속 클랙스 상속

---
+++ Company/Company/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Company
+{
+    class Emp {
+        string name;
+        void GotoOffice() { }
+    }
+
+    class Programmer : Emp { }  // Emp 클래스 상속
+
+    class EmpTest {
+
+        static void Main()
+        {
+            Programmer p = new Programmer();
+
+            if (p is Emp)
+            {
+                Console.WriteLine("Programmer is Emp");
+
+            }
+            else
+            {
+                Console.WriteLine("Programmer is not Emp");
+            }
+
+        }
+
+    }
+        
+
+}
