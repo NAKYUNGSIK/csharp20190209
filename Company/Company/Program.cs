75092ccd7ccac3c233957b9bbafc08a14d671d4f
나경식 <autona@naver.com>
2019-02-09 14:53:35 +0900

    형변환

--- Company/Company/Program.cs
+++ Company/Company/Program.cs
@@ -2,19 +2,22 @@
 
 namespace Company
 {
-    class Emp {
+/*    class Emp {
         string name;
         void GotoOffice() { }
     }
-
-    class Programmer : Emp { }  // Emp 클래스 상속
+*/
+        class Emp { }
+        class Programmer : Emp { }  // Emp 클래스 상속
 
     class EmpTest {
 
-        static void Main()
-        {
+        static void Main() {
+            Emp e = new Emp();
             Programmer p = new Programmer();
-
+            Emp e1 = p as Emp;   // p를 Emp로 형변환 (상위타입 형변환은 된다) 변환안될시 null값 반환.
+            Emp e2 = (Emp)p;  // 위와 같은데 아닐시 에러나니까 쓰지 말라..
+/*
             if (p is Emp)
             {
                 Console.WriteLine("Programmer is Emp");
@@ -24,10 +27,9 @@ namespace Company
             {
                 Console.WriteLine("Programmer is not Emp");
             }
-
+*/
         }
 
     }
         
-
 }
