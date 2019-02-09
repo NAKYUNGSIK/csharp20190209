e10dd1358105d8f8f0366e2db248cadec3c41f88
나경식 <autona@naver.com>
2019-02-09 15:11:42 +0900

    null 오퍼레이션

---
+++ NullOperater/NullOperater/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NullOperater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = null;
+            object b = "abc";
+
+            object c = a ?? b;
+            Console.WriteLine(c);
+
+            object d;
+
+            if (a != null)
+            {
+
+                d = a;
+            }
+            else {
+
+                d = b;
+            }
+
+            Console.WriteLine(d);
+
+            object e;
+            e = (a != null) ? a : d;
+
+            Console.WriteLine(e);
+        }
+    }
+}
