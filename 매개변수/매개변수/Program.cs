062b282c33994479046efb9d855e3fad015a75a4
나경식 <autona@naver.com>
2019-02-10 09:43:29 +0900

    가변 매개변수

---
+++ 매개변수/매개변수/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 매개변수
+{
+    class Program
+    {
+        private static void OnjSum(string title, params int[] num) {
+            int sum = 0;
+            Console.WriteLine("Sum of {0}", title);
+
+            foreach (int i in num) sum += 1;
+
+            Console.WriteLine("Sum :: {0}\n", num);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] onjArray = new int[2] { 1, 2 };
+            OnjSum("숫자하나", 1);
+            OnjSum("숫자둘", 1, 2);
+            OnjSum("숫자셋", 1, 2, 3);
+            OnjSum("배열", onjArray);
+        }
+    }
+}
