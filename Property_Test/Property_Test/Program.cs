e5b75a355200122512e78b3d037abc5b443d79ed
나경식 <autona@naver.com>
2019-02-10 10:34:58 +0900

    property 테스트

---
+++ Property_Test/Property_Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Property_Test
+{
+    class Emp : Object   {
+
+        private  int empno;
+
+        public void SetEmpno(int empno) //Setter
+        {
+            this.empno = empno;  // this.empno = private int empno 이고 empno = SetEmpno(int empno) 
+        }
+
+        public int GetEmpno() //Getter
+        {
+            return this.empno;
+        }
+
+        public override string ToString()  // override 는 재정의
+        {
+            return $"[사원 : {empno }]";
+        }
+    }
+    class EmpTest
+    {
+        static void Main(string[] args)
+        {
+            Emp e = new Emp(); e.SetEmpno(111);
+            Console.WriteLine(e.GetEmpno ());
+            Console.WriteLine(e);
+            Console.WriteLine(e.ToString ());
+
+        }
+    }
+}
