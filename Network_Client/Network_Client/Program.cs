0fd2235335e7bdf1c095fe31c6f98588929d4796
나경식 <autona@naver.com>
2019-02-17 15:00:39 +0900

    Tcp Client

--- Network_Client/Network_Client/Program.cs
+++ Network_Client/Network_Client/Program.cs
@@ -10,8 +10,7 @@ class TcpClientTest
         TcpClient client = null;
         try
         {
-            //LocalHost에 지정 포트로 TCP Connection을 생성하고 데이터를 송수신 하기
-             //위한 스트림을 얻는다.
+            //LocalHost에 지정 포트로 TCP Connection을 생성하고 데이터를 송수신 하기위한 스트림을 얻는다.
             client = new TcpClient();
             client.Connect("localhost", 5001);
             NetworkStream stream = client.GetStream();
